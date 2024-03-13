97af33892eb0eb80f4c23014cc0b1569c4c3af26
saifulislam05 <saiful.bijulia@gmail.com>
2024-03-13 11:46:15 +0530

    Feat: added encryption and token system
    Added password Encryption system while Register and added sending token as user info when login

--- controllers/user.js
+++ controllers/user.js
@@ -1,16 +1,22 @@
-import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
 import userModel from "../models/user.js";
 
+
 const register = async (req, res) => {
+  const hash = bcrypt.hashSync(req.body.password, 10);
   const newUser = new userModel({
     ...req.body,
+    password: hash,
   });
   try {
-    const newlyRegisteredUser = await newUser.save();
+    // const newlyRegisteredUser = await newUser.save();
+    await newUser.save();
     res.json({
       success: true,
       message: "user registered successfully",
-      result: newlyRegisteredUser,
     });
   } catch (error) {
     res.status(500).json({
@@ -22,30 +28,39 @@ const register = async (req, res) => {
 };
 const login = async (req, res) => {
   console.log(req.body);
-  
-      const user = await userModel.findOne({ email: req.body.email });
-      if (!user) {
-        return res.status(404).json({
-          success: false,
-          message: "user not registered with this email",
-        });
-    }
-    const isPasswordCorrect = req.body.password === user.password;
-    if (!isPasswordCorrect) {
-        return res.status(401).json({
-          success: false,
-          message: "wrong password",
-          
-        });
-    }
-    res.json({
-      success: true,
-      message: "login successed",
-      result: user,
+
+  const user = await userModel.findOne({ email: req.body.email });
+  if (!user) {
+    return res.status(404).json({
+      success: false,
+      message: "user not registered with this email",
     });
-  
+  }
+  const isPasswordCorrect = bcrypt.compareSync(req.body.password,user.password) ;
+  if (!isPasswordCorrect) {
+    return res.status(401).json({
+      success: false,
+      message: "wrong password",
+    });
+    }
+    
+    const expiry = Math.floor(Date.now() / 1000) + 7200 // 2hr from now
+    
+    const payload = {
+        id: user._id,
+        firstname:user.firstname,
+        lastname: user.lastname,
+        role: user.role,
+        exp:expiry
+    }
+
+    const token = jwt.sign(payload, process.env.TOKEN_SECRET_KEY);
+
+  res.json({
+    success: true,
+      message: "login successed",
+      token:token
+  });
 };
 
-
-
-export { register,login };
+export { register, login };
