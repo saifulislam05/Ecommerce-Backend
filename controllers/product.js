bc0042a48032fd0207b77b5027c444f1c72ff51e
saifulislam05 <saiful.bijulia@gmail.com>
2024-03-14 05:49:02 +0530

    Feat: product updation endpoint done

--- controllers/product.js
+++ controllers/product.js
@@ -1,20 +1,35 @@
-import productModel from "../models/product.js"
-
+import productModel from "../models/product.js";
 
 const createProduct = async (req, res) => {
-    try {
-        const newProduct = await productModel.create(req.body);
-        res.json({
-          success: true,
-          message: "product created successfully",
-        });
-    } catch (error) {
-        res.status(500).json({
-        success: true,
-        message:`there is problem to create product`
-    })
-    }
-    
-}
+  try {
+    const newProduct = await productModel.create(req.body);
+    res.json({
+      success: true,
+      message: "product created successfully",
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: true,
+      message: `there is problem to create product`,
+    });
+  }
+};
 
-export {createProduct}
+const updateProduct = async (req, res) => {
+  console.log("params", req.params);
+  console.log("body", req.body);
+  try {
+    await productModel.findByIdAndUpdate(req.params.productid, req.body);
+    res.json({
+      success: true,
+      message: "product updated successfully",
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: true,
+      message: `there is problem to create product`,
+    });
+  }
+};
+
+export { createProduct, updateProduct };
--- routes/product.js
+++ routes/product.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { createProduct } from "../controllers/product.js";
+import { createProduct, updateProduct } from "../controllers/product.js";
 import authCheck from "../middlewares/auth.js";
 
 
 const router = express.Router();
 
 router.post("/",authCheck(["Admin","Seller"]), createProduct);
+router.patch("/:productid",authCheck(["Admin","Seller"]), updateProduct);
 
 
 export default router;
